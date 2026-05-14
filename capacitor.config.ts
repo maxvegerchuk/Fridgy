16013351d3bdd9608cf4c08a55e9d12c2982df6f
maxvegerchuk <max.vegercuk.com>
2026-05-14 14:42:51 +0300

    app integration fix

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.maxvegerchuk.fridgy',
+  appName: 'Fridgy',
+  webDir: 'dist'
+};
+
+export default config;
